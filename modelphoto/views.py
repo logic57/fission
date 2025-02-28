a784c80e82b723b171e975039dfccf38aa17f572
Example User <example@example.com>
2025-02-28 14:35:29 +0800

    添加模特图制作功能页面和相关配置

--- modelphoto/views.py
+++ modelphoto/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 def model_photo_maker(request):
     """模特图制作页面的视图函数"""
-    return render(request, 'modelphoto/index.html')
+    return render(request, 'modelphoto/model_photo_maker.html')
